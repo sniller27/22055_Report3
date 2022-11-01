9e5d757d5a0433330183b223ee739cfe167c3c14
Jonas F. Jakobsen <jonasfalke@gmail.com>
2022-11-01 14:12:06 +0100

    2a: dynamic thresholding initiated. only working with certain images due to findpeaks function sensitivity

--- report3_2a.m
+++ report3_2a.m
@@ -29,30 +29,57 @@ for i=1:images_count
 end
 
 %% Image histograms (imhist seems to lag have an annoying scale bar)
+rows = 5;
+columns = 5;
+
 figure;
-subplot(2,1,1);
 hold on;
 title('Image histograms');
-for i=1:images_count
+thresholds = zeros(1,images_count);
+for i=1:3
+subplot(columns,rows,i);
 gray_image = cell2mat(x_seg(i));
 [H,W] = size(gray_image);
 M = H*W;
 %%tt = double(reshape(cell2mat(x_seg(i)), M, 1));
-histogram(double(reshape(gray_image, M, 1)));
+hout = histogram(double(reshape(gray_image, M, 1)));
+hold on;
+
+histLine = hout.Values(1:2:end);
+histLineX = hout.BinEdges(2:2:end);
+
+% stuff
+% [val,troughloc] = findpeaks(-histLine,histLineX,'MinPeakProminence',1000);
+% plot(histLineX,-histLine);
+% plot(troughloc,-val,'*');
+
+[val,troughloc] = findpeaks(-histLine,histLineX,'MinPeakProminence',1);
+
+counter = 1;
+
+while(length(val)>1)
+    [val,troughloc2] = findpeaks(-histLine,histLineX,'MinPeakProminence',1+counter);
+    counter = counter+1;
 end
+
+thresholds(i) = (troughloc2);
+plot(histLineX,histLine);
+plot(troughloc2,-val,'*');
+end
+
 hold off;
 
-subplot(2,1,2);
-hold on;
-title('Image histograms equalized');
-for i=1:images_count
-gray_image = histeq(cell2mat(x_seg(i)));
-[H,W] = size(gray_image);
-M = H*W;
-%%tt = double(reshape(cell2mat(x_seg(i)), M, 1));
-histogram(double(reshape(gray_image, M, 1)));
-end
-hold off;
+% subplot(2,1,2);
+% hold on;
+% title('Image histograms equalized');
+% for i=1:images_count
+% gray_image = histeq(cell2mat(x_seg(i)));
+% [H,W] = size(gray_image);
+% M = H*W;
+% %%tt = double(reshape(cell2mat(x_seg(i)), M, 1));
+% histogram(double(reshape(gray_image, M, 1)));
+% end
+% hold off;
 
 %% Thresholding
 rows = 5;
@@ -67,7 +94,7 @@ hold on;
 for i=1:images_count
     
     gray_image = cell2mat(x_seg(i));
-    binI = (gray_image > T1) & (gray_image < mean(mean(gray_image))); % thresholding
+    binI = (gray_image > T1) & (gray_image < thresholds(i)); % thresholding
     
     subplot(columns,rows,i);
     
@@ -76,19 +103,28 @@ for i=1:images_count
 end
 hold off;
 
-%%
-T1 = 50; % Lower limit (cancer melanoma)
-T2 = 200; % Upper limit (cancer melanoma)
+% T1 = 50; % Lower limit (cancer melanoma)
+% T2 = 200; % Upper limit (cancer melanoma)
+% 
+% figure;
+% sgtitle('Histogram equalized images thresholded');
+% hold on;
+% for i=1:images_count
+%     
+%     gray_image = histeq(cell2mat(x_seg(i)));
+%     binI = (gray_image > T1) & (gray_image < T2); % thresholding
+%     subplot(columns,rows,i);
+%     imshow(binI);
+%     
+% end
+% hold off;
 
+%%
+no = 1; % 6, 7
+
+%gray_image = cell2mat(x_seg(no));
+gray_image = histeq(cell2mat(x_seg(no)));
+binI = (gray_image > 0) & (gray_image < 20); % thresholding (smaller ones)
+%binI = (gray_image > 0) & (gray_image < 130); % thresholding (big ones)
 figure;
-sgtitle('Histogram equalized images thresholded');
-hold on;
-for i=1:images_count
-    
-    gray_image = histeq(cell2mat(x_seg(i)));
-    binI = (gray_image > T1) & (gray_image < T2); % thresholding
-    subplot(columns,rows,i);
-    imshow(binI);
-    
-end
-hold off;
+imshow(binI)
